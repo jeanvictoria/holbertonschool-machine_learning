e82adf8a61a4edeb8faed56fdad5ab4c29cbb2c5
jeanvictoria <jean.vic8@gmail.com>
2020-07-18 23:30:34 -0500

    task 14

---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""14-saddle_up.py contain the np_matmul function"""
+import numpy as np
+
+
+def np_matmul(matrix1, matrix2):
+    """performs matrix multiplication"""
+    return np.matmul(matrix1, matrix2)

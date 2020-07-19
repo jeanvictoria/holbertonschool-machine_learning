95c855d561523010095736b7b82e6b42e323cc94
jeanvictoria <jean.vic8@gmail.com>
2020-07-18 23:28:41 -0500

    task 11

---
+++ math/0x00-linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""11-the_western_exchange.py contain the np_transpose function"""
+
+
+def np_transpose(matrix):
+    """transposes matrix """
+    return matrix.transpose()

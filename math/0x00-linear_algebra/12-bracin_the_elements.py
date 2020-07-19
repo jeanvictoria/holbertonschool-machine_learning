9abe93b4108457f7930f5360e043d8a42e314c91
jeanvictoria <jean.vic8@gmail.com>
2020-07-18 23:29:22 -0500

    task 12

---
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""12-bracin_the_elements.py contain the np_elementwise function"""
+
+
+def np_elementwise(matx1, matx2):
+    """performs element-wise addition, subtraction,
+       multiplication and division"""
+    return matx1 + matx2, matx1 - matx2, matx1 * matx2, matx1 / matx2

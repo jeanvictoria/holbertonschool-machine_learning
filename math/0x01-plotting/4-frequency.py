9b2c7769811fa323b5d6952b481331366a0243a9
jeanvictoria <jean.vic8@gmail.com>
2020-07-21 10:03:19 -0500

    task 4

---
+++ math/0x01-plotting/4-frequency.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(5)
+student_grades = np.random.normal(68, 15, 50)
+
+bin = np.arange(0, max(student_grades), 10)
+plt.hist(student_grades, bins=bin, edgecolor='black')
+ybin = np.arange(0, len(student_grades), 5)
+plt.yticks(ybin)
+plt.xlabel('Grades')
+plt.ylabel('Number of Students')
+plt.title('Project A')
+plt.xlim(0, 100)
+plt.ylim(0, 30)
+plt.show()

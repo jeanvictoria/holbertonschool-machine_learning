af14d04c5ba437a5cd9c77d93b397b066019c878
jeanvictoria <jean.vic8@gmail.com>
2020-08-24 22:02:51 -0500

    task 1

---
+++ supervised_learning/0x04-error_analysis/1-sensitivity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""calculates sensitivity for each class in a confusion matrix"""
+
+import numpy as np
+
+
+def sensitivity(confusion):
+    """
+    confusion: confusion numpy.ndarray of shape (classes, classes)
+    classes: num of classes
+    """
+    sensitivity = np.diagonal(confusion) / (np.sum(confusion, axis=1))
+    return sensitivity

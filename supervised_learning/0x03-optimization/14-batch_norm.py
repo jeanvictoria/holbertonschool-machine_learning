97e044ca3420190cb7505f6272f48a397303fa0c
jeanvictoria <jean.vic8@gmail.com>
2020-08-27 22:31:01 -0500

    task 14

---
+++ supervised_learning/0x03-optimization/14-batch_norm.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+function creates  a batch normalization layer for NN
+in tensorflow
+"""
+import tensorflow as tf
+
+
+def create_batch_norm_layer(prev, n, activation):
+    """
+    prev: activated output of previous layer
+    n: num of nodes in layer to be created
+    activation: activation function used on output of layer
+    """
+    init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    mm = tf.layers.Dense(units=n, activation=None,
+                         kernel_initializer=init)
+    batch_mean, batch_var = tf.nn.moments(mm(prev), axes=[0])
+
+    beta = (tf.get_variable("beta", [n],
+                            initializer=tf.zeros_initializer(),
+                            trainable=True))
+    gamma = (tf.get_variable("gamma", [n],
+                             initializer=tf.ones_initializer(),
+                             trainable=True))
+    normlayer = (tf.nn.batch_normalization(mm(prev), mean=batch_mean,
+                                           variance=batch_var, offset=beta,
+                                           scale=gamma,
+                                           variance_epsilon=1e-8))
+    return activation(normlayer)

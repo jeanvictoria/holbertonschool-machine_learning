337e7f0b5d01da5d0b27f0a20afd4f9ee96438a6
jeanvictoria <jean.vic8@gmail.com>
2020-10-09 15:17:43 -0500

    task 2

--- supervised_learning/0x07-cnn/2-conv_backward.py
+++ supervised_learning/0x07-cnn/2-conv_backward.py
@@ -1,88 +1,91 @@
 #!/usr/bin/env python3
-"""
-function performs backProp over a convolutional layer
-"""
+"""Contains the conv_backward function"""
 
 import numpy as np
 
 
 def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
     """
-     dZ: np.ndarray, partial derivatives
-        m: num examples
-        h_new: output height
-        w_new: output weight
-        c_new: output channels
-    A_prev: np.ndarray, prev output layer
-        m: num examples
-        h_prev: prev height
-        w_prev: prev width
-        c_prev: prev channels
-    w: np.ndarray, kernels
-        kh: filter height
-        kw: filter width
-        c_prev: prev channels
-        c_new: output channels
-     b: (1, 1, 1, c_new)
-     padding: 'same' or 'valid'
-     stride: (sh, sw)
-     Return: dA_prev, dW, db
-     """
-    # retrive dimensions
+    Performs back propagation over a convolutional layer of a neural network
+    :param dZ: numpy.ndarray of shape (m, h_new, w_new, c_new)
+        containing the partial derivatives with respect to the unactivated
+        output of the convolutional layer
+        m is the number of examples
+        h_new is the height of the output
+        w_new is the width of the output
+        c_new is the number of channels in the output
+    :param A_prev: numpy.ndarray of shape (m, h_prev, w_prev, c_prev)
+        containing the output of the previous layer
+        m is the number of examples
+        h_prev is the height of the previous layer
+        w_prev is the width of the previous layer
+        c_prev is the number of channels in the previous layer
+    :param W: numpy.ndarray of shape (kh, kw, c_prev, c_new)
+        containing the kernels for the convolution
+        kh is the filter height
+        kw is the filter width
+        c_prev is the number of channels in the previous layer
+        c_new is the number of channels in the output
+    :param b: numpy.ndarray of shape (1, 1, 1, c_new)
+        containing the biases applied to the convolution
+    :param padding: string that is either same or valid,
+        indicating the type of padding used
+    :param stride: tuple of (sh, sw) containing the strides for the convolution
+        sh is the stride for the height
+        sw is the stride for the width
+    :return: the partial derivatives with respect to
+        the previous layer (dA_prev),
+        the kernels (dW),
+        and the biases (db), respectively
+    """
+    # Retrieving dimensions from dZ
     m, h_new, w_new, c_new = dZ.shape
-    m, h_prev, w_prev, c_prev = A_prev.shape
-    kh, kw, c_prev, c_new = W.shape
-    sh = stride[0]
-    sw = stride[1]
 
-    # calculate padding for 'valid' and 'same'
-    if padding == 'valid':
-        ph = 0
-        pw = 0
+    # Retrieving dimensions from A_prev shape
+    _, h_prev, w_prev, c_prev = A_prev.shape
+
+    # Retrieving dimensions from W's shape
+    kh, kw, _, _ = W.shape
+
+    # Retrieving stride
+    sh, sw = stride
+
+    # Setting padding for valid
+    pw, ph = 0, 0
+
+    # bias calculation
+    db = np.sum(dZ, axis=(0, 1, 2), keepdims=True)
+
+    # Setting padding for same
     if padding == 'same':
-        ph = np.ceil(((sh * h_prev) - sh + kh - h_prev) / 2)
-        ph = int(ph)
-        pw = np.ceil(((sw * w_prev) - sw + kw - w_prev) / 2)
-        pw = int(pw)
+        ph = int(np.ceil(((h_prev-1)*sh+kh-h_prev)/2))
+        pw = int(np.ceil(((w_prev-1)*sw+kw-w_prev)/2))
 
-    # initializing output: dA_prev, dW, db
-    dA_prev = np.zeros(A_prev.shape)
+    # pad images
+    A_prev = np.pad(A_prev,
+                    pad_width=((0, 0),
+                               (ph, ph),
+                               (pw, pw),
+                               (0, 0)),
+                    mode='constant', constant_values=0)
+
+    # Initializing dX, dW with the correct shapes
+    dA = np.zeros(A_prev.shape)
     dW = np.zeros(W.shape)
-    db = np.zeros(b.shape)
 
-    # pad previous images
-    padA_prev = np.pad(A_prev, [(0, 0), (ph, ph), (pw, pw), (0, 0)],
-                       mode='constant', constant_values=0)
-    paddA_prev = np.pad(dA_prev, [(0, 0), (ph, ph), (pw, pw), (0, 0)],
-                        mode='constant', constant_values=0)
+    # Looping over vertical(h) and horizontal(w) axis of the output
+    for z in range(m):
+        for y in range(h_new):
+            for x in range(w_new):
+                # over every channel
+                for v in range(c_new):
+                    aux_W = W[:, :, :, v]
+                    aux_dz = dZ[z, y, x, v]
+                    dA[z, y*sh: y*sh+kh, x*sw: x*sw+kw, :] += aux_dz * aux_W
+                    aux_A_prev = A_prev[z, y*sh: y*sh+kh, x*sw: x*sw+kw, :]
+                    dW[:, :, :, v] += aux_A_prev * aux_dz
 
-    # vectorize
-    imagesVector = np.arange(0, m)
+    # subtracting padding
+    dA = dA[:, ph:dA.shape[1]-ph, pw:dA.shape[2]-pw, :]
 
-    # Looping engine: loop over m, h_new, w_new, c_new
-    for n in range(m):
-        # select training example i
-        a_padA_prev = padA_prev[n, :, :, :]
-        da_paddA_prev = paddA_prev[n, :, :, :]
-
-        for i in range(h_new):
-            for j in range(w_new):
-                for k in range(c_new):
-                    # finding the corners
-                    istart = i * sh
-                    iend = istart + kh
-                    jstart = j * sw
-                    jend = jstart + kw
-
-                    # slice from previous layer
-                    aSlice = a_padA_prev[istart:iend, jstart:jend, :]
-
-                    # update gradients
-                    da_paddA_prev[
-                        istart:iend, jstart:jend, :] += W[:, :, :, k] * dZ[
-                            n, i, j, k]
-
-                    dW[:, :, :, k] += aSlice * dZ[n, i, j, k]
-                    db[:, :, :, k] += dZ[n, i, j, k]
-    assert(dA_prev.shape == A_prev.shape)
-    return dA_prev, dW, db
+    return dA, dW, db

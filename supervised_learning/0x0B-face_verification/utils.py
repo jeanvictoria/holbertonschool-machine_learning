ffa0441465a91a0e1767e78a603b5704fdb5a46d
jeanvictoria <jean.vic8@gmail.com>
2020-10-09 08:50:28 -0500

    utils.py

--- supervised_learning/0x0B-face_verification/utils.py
+++ supervised_learning/0x0B-face_verification/utils.py
@@ -1,132 +1,126 @@
 #!/usr/bin/env python3
-"""
-Face Verification
-"""
+""" Face Varification System """
 
-import csv
-import cv2
 import numpy as np
 import os
 import glob
+import cv2
+import csv
 
 
 def load_images(images_path, as_array=True):
+    """ Loading the images in a RGB format from a directory or file by
+        alphabetical order  by filename.
+    Arg:
+        images_path: path to a directory from which to load images
+        as_array: boolean indicating the images should be
+                  loaded as one numpy.ndarray or not
+               If True, load the imgs numpy.ndarray of shape (m, h, w, c)
+               If False, load the imgs as a list of individual np.ndarrays
+    Returns: images, filenames
+        images: is either a list/numpy.ndarray of all images
+        filenames: is a list of the filenames of each image
     """
-    function loads images from directory or file
-    image_path: path  to a directory  of images
-    as_array: boolean indicating the images to be loaded
-             images loaded as one numpy.ndarray
-    Boolean factors: if true
-                    images loaded as numpy.ndarray
-                    shape(m, h, w, c)
-                    if false
-                    images loaded as list of indvidual
-                    numpy.ndarray
-        m: number of imagges
-        h: height
-        w: width
-        c: num channels
-    :return: images, filenames
-        images: RGB format
-                as list  of arrays or single array
-        filenames: alphabetical Order
-                as list of flienames assoc w/ each image
-    """
-    # image path
-    imagePaths = glob.glob(images_path + "/*")
+    if os.path.exists(images_path):
+        if os.path.isdir(images_path):
+            # if images_path is a directory, else if it is a filename
+            path = images_path + '/*.jpg'
+            image_paths = glob.glob(path, recursive=False)
+            image_paths.sort()
+        else:
+            image_paths = images_path
 
-    # get image names
-    imageNames = []
-    for imPath in imagePaths:
-        imageNames.append(imPaths.split('/')[-1])
+    loaded_images = []
+    images_names = []
 
-    imageIndex = np.argsort(imageNames)
+    for i, img in enumerate(image_paths):
+        src = cv2.imread(img)
+        # convert the image into a RGB format
+        image = cv2.cvtColor(src, cv2.COLOR_BGR2RGB)
+        # extracting the image name of the file_path
+        name = image_paths[i].split('/')[-1]
+        loaded_images.append(image)
+        images_names.append(name)
 
-    # read and color images
-    imageOrig = []
-    for pics in imagePaths:
-        imageOrig.append(cv2.imread(pics))
-    for pics in imageOrig:
-        imageOrig.append(cv2.cvtColor(pics, cv2.COLOR_BGR2RGB))
-
-    images = []
-    filenames = []
-
-    # append images and file names
-    for m in imageIndex:
-        images.append(imageOrig[m])
-        filenames.append(imageNames[m])
-
-    # as_array
     if as_array:
-        images = np.stack(images, axis=0)
+        loaded_images = np.stack(loaded_images, axis=0)
 
-    return images, filenames
+    return (loaded_images, images_names)
 
 
 def load_csv(csv_path, params={}):
+    """ loads the content of a CVS file a lists of lists
+    Arg:
+        csv_path is the path to the csv to load
+        params are the parameters to load the csv with
+    Returns: list of lists representing the contents found in csv_path
     """
-    :param csv_path: loads contents of csv files as list
-    :param params: parameters to load csv with
-    :return: list of lists rep contents found in csv_path
-    """
-    csvList = []
-    with open(csv_path, 'r') as csvFile:
-        csvReader = csv.reader(csvFile, params)
-        for row in csvReader:
-            csvList.append(row)
-    return csvList
+    csv_content = []
+
+    with open(csv_path, encoding="utf-8") as csv_file:
+        # used csv (comma separated values file)
+        csv_reader = csv.reader(csv_file, params)
+        for line in csv_reader:
+            csv_content.append(line)
+    return csv_content
 
 
 def save_images(path, images, filenames):
-    """
-    function that saves images images to a path
-    :path: path to directory in which images should be saved
-    :images: list/numpy.ndarray; images to save
-    :filenames: list of filenames of the images to save
-    :return: True on success
-            False on failure
+    """saves images to a specific path
+    Arg:
+        path: path to the directory in which the images should be saved
+        images: list/numpy.ndarray of images to save
+        filenames: list of filenames of the images to save
+    Returns: True on success and False on failure
     """
     if os.path.exists(path):
-        for imag, imagName in zip(images, filenames):
-            pict = cv2.cvtColor(imag, cv2.COLOR_BGR2RGB)
-            cv2.imwrite('./' + path + '/' + imagName, pict)
+        for img, name in zip(images, filenames):
+            # convert the image into a RGB format
+            image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            cv2.imwrite('./' + path + '/' + name, image)
         return True
     else:
         return False
 
 
 def generate_triplets(images, filenames, triplet_names):
+    """ generates the triplets
+    Arg:
+        images: np.ndarray shape (n, h, w, 3) with images in the dataset
+        filenames: list leng n with the corresponding filenames for images
+        triplet_names: list of lists where each sublist contains the
+                 filenames of an anchor, positive, and negative image
+    Returns: a list [A, P, N]
+       - A np.ndarray shape (m, h, w, 3) has anchor images for all m triplets
+       - P np.ndarray (m, h, w, 3) has the positive images for all m triplets
+       - N np.ndarray (m, h, w, 3) has the negative images for all m triplets
     """
-    function that generates triplets
-    :param images:
-    :param filenames:
-    :param triplet_names:
-    :return: list [A, P, N]
-    A is a numpy.ndarray shape (m, h, w, 3)
-        containing anchor images for  m triplets
-    P is a numpy.ndarray shape (m, h, w, 3)
-        containing positive images for m triplets
-    N is a numpy.ndarray shape (m, h, w, 3)
-        containing negative images for m triplets
-    """
+    inds_a, inds_p, inds_n = [], [], []
 
-    imagNames = [filenames[m].split('.')[0] for m in range(len(filenames))]
+    names = [filenames[i].split('.')[0] for i in range(len(filenames))]
+    # Reeplace values with differents characters
+    for triplet_name in triplet_names:
+        for name in triplet_name:
+            if name not in names:
+                # Reeplace special characters as í, é, ñ
+                new_value = name.encode('utf-8').decode('utf-8')
+                new_value = new_value.replace('eÌ', 'é')
+                new_value = new_value.replace('iÌ', chr(105) + chr(769))
+                new_value = new_value.replace('nÌƒ', chr(110)+chr(771))
+                new_value = new_value.replace('\x81', '')
+                new_value = new_value.replace('\x81', '')
+                # Find the name of the triplet in names array and replace it
+                triplet_name[triplet_name.index(name)] = new_value
 
-    ancNames = [names[0] for names in triplet_names]
-    posNames = [names[1]for names in triplet_names]
-    negNames = [names[2]for names in triplet_names]
+    # Create the list with indices of the values
+    for triplet_name in triplet_names:
+        inds_a.append(names.index(triplet_name[0]))
+        inds_p.append(names.index(triplet_name[1]))
+        inds_n.append(names.index(triplet_name[2]))
 
-    a_imgName = [m for m in range(len(imagNames)) if imagNames[m] in ancNames]
-    p_imgName = [m for m in range(len(imagNames)) if imagNames[m] in posNames]
-    n_imgName = [m for m in range(len(imagNames)) if imagNames[m] in negNames]
-
-    A = images[a_imgName]
-    P = images[p_imgName]
-    N = images[n_imgName]
-
-    print(A.shape)
-    print(P.shape)
-    print(N.shape)
+    # Create arrays with the images
+    A = images[inds_a]
+    P = images[inds_p]
+    N = images[inds_n]
 
     return [A, P, N]

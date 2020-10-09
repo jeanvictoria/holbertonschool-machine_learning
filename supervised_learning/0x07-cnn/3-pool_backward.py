339bd45a34989e2bbb5c1313e711eb242a012b52
jeanvictoria <jean.vic8@gmail.com>
2020-10-09 15:14:38 -0500

    task 3

--- supervised_learning/0x07-cnn/3-pool_backward.py
+++ supervised_learning/0x07-cnn/3-pool_backward.py
@@ -1,59 +1,67 @@
 #!/usr/bin/env python3
-"""
-function performs back propagatino over pooling layer
-"""
+"""Contains the pool_forward function"""
 
 import numpy as np
 
 
 def pool_backward(dA, A_prev, kernel_shape, stride=(1, 1), mode='max'):
     """
-    dZ: np.ndarray, partial derivatives
-        m: num examples
-        h_new: output height
-        w_new: output weight
-        c_new: output channels
-    A_prev: np.ndarray, prev output layer
-        m: num examples
-        h_prev: prev height
-        w_prev: prev width
-        c_prev: prev channels
-    kernel_shape: tuple, (kh, kw)
-    stride: tuple, (sh, sw)
-    mode: max or min
+    Performs forward propagation over a convolutional layer of a neural network
+    :param dA: numpy.ndarray of shape (m, h_new, w_new, c_new)
+    containing the partial derivatives with respect to the output
+        of the pooling layer
+        m is the number of examples
+        h_new is the height of the output
+        w_new is the width of the output
+        c_new is the number of channels
+    :param A_prev: numpy.ndarray of shape (m, h_prev, w_prev, c_prev)
+        containing the output of the previous layer
+        m is the number of examples
+        h_prev is the height of the previous layer
+        w_prev is the width of the previous layer
+        c_prev is the number of channels in the previous layer
+    :param kernel_shape: is a tuple of (kh, kw)
+        containing the size of the kernel for the pooling
+        containing the kernels for the convolution
+        kh is the filter height
+        kw is the filter width
+    :param stride: tuple of (sh, sw) containing the strides for the convolution
+        sh is the stride for the height
+        sw is the stride for the width
+    :param mode: string containing either max or avg,
+        indicating whether to perform maximum or average pooling, respectively
+    :return: partial derivatives with respect to the previous layer (dA_prev)
     """
-    # retrive dimensions
+    # Retrieving dimensions from dA
     m, h_new, w_new, c_new = dA.shape
+
+    # Retrieving dimensions from A_prev shape
     m, h_prev, w_prev, c_prev = A_prev.shape
-    kh = kernel_shape[0]
-    kw = kernel_shape[1]
-    sh = stride[0]
-    sw = stride[1]
 
-    # initializing output: dA_prev, dW, db
-    dA_prev = np.zeros(A_prev.shape)
+    # Retrieving dimensions from kernel_shape
+    kh, kw = kernel_shape
 
-    # vectorize
-    poImagesVector = np.arange(0, m)
+    # Retrieving stride
+    sh, sw = stride
 
-    # max or min mode
-    if (mode == 'avg'):
-        activation = np.average
-    if (mode == 'max'):
-        activation = np.max
+    # Initialize the output with zeros
+    dA_prev = np.zeros_like(A_prev, dtype=dA.dtype)
 
-    # Looping engine: loop over m, h_new, w_new, c_new
-    for n in range(m):
-        a_prev = A_prev[n, :, :, :]
-
-        for i in range(h_new):
-            for j in range(w_new):
-                for k in range(c_new):
-                    # finding the corners
-                    istart = i * sh
-                    iend = istart + kh
-                    jstart = j * sw
-                    jend = jstart + kw
-                    dA_prev[poImagesVector, i, j, k] = activation(
-                        (dA_prev[n, i, j, k] + dA[n, i, j, k]))
+    for z in range(m):
+        for y in range(h_new):
+            for x in range(w_new):
+                for v in range(c_new):
+                    pool = A_prev[z, y * sh:(kh+y*sh), x * sw:(kw+x*sw), v]
+                    dA_aux = dA[z, y, x, v]
+                    if mode == 'max':
+                        z_mask = np.zeros(kernel_shape)
+                        _max = np.amax(pool)
+                        np.place(z_mask, pool == _max, 1)
+                        dA_prev[z, y * sh:(kh + y * sh),
+                                x * sw:(kw+x*sw), v] += z_mask * dA_aux
+                    if mode == 'avg':
+                        avg = dA_aux / kh / kw
+                        o_mask = np.ones(kernel_shape)
+                        dA_prev[z, y * sh:(kh + y * sh),
+                                x * sw:(kw+x*sw), v] += o_mask * avg
     return dA_prev

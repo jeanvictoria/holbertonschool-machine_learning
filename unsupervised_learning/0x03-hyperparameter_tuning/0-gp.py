7388dbf5a3234fd5506017540682214d33f4db1d
Jean Victoria <jean.vic8@gmail.com>
2020-11-20 22:36:36 -0500

    task 0

--- unsupervised_learning/0x03-hyperparameter_tuning/0-gp.py
+++ unsupervised_learning/0x03-hyperparameter_tuning/0-gp.py
@@ -1,49 +1,54 @@
 #!/usr/bin/env python3
 """
-Class the represents a noisless 1D Gaussian Process
+Class GaussianProcess
+That represents a noiseless 1D Gaussian process
 """
 import numpy as np
 
 
-class GaussianProcess:
+class GaussianProcess():
+    """
+    Class that represents a noiseless 1D Gaussian process
+    """
 
-    """ Class reoresents noisless 1D Gaussian Process"""
-    def __init__(self, X_init, Y_init, L=1, sigma_f=1):
+    def __init__(self, X_init, Y_init, l=1, sigma_f=1):
         """
-        class comstructor
-        :param self:
-        :param X_init: np.ndarray (t,1) imputs sampled with black-box function
-        :param Y_init:np.ndarray (t, 1) outputs of the black box function
-                        for X_init
-        :param L: length parameter for the kernel
-        :param sigma_f: standard deviation given output
-        :return: covariance np.ndarray (m,n)
+        Constructor
+        Arguments:
+         - X_init is a numpy.ndarray of shape (t, 1) representing
+            the inputs already sampled with the black-box function
+         - Y_init is a numpy.ndarray of shape (t, 1) representing
+            the outputs of the black-box function for each input in X_init
+         - t is the number of initial samples
+         - l is the length parameter for the kernel
+         - sigma_f is the standard deviation given to the output
+            of the black-box function
+        Public instance attributes:
+         - X corresponding to the respective constructor inputs
+         - Y corresponding to the respective constructor inputs
+         - l corresponding to the respective constructor inputs
+         - sigma_f  corresponding to the respective constructor inputs
+         - K  representing the current covariance kernel matrix
+            for the Gaussian process
         """
-        # t = 0
-        # X_init = np.ndarray(shape=(t, 1))
-        # Y_init = np.ndarray(shape=(t, 1))
         self.X = X_init
         self.Y = Y_init
-        self.L = L
+        self.l = l
         self.sigma_f = sigma_f
-        self.K = self.kernel(self.X, self.X)
+        self.K = self.kernel(X_init, X_init)
 
     def kernel(self, X1, X2):
         """
-        calculates the covariance kernel matrix between two
-        Matrices
-        :param X1: np.ndarray, (m, 1)
-        :param X2: np.ndarray, (n, 1)
-        :return: covariance matrix (m, n)
+        Public instance method that calculates the covariance kernel matrix
+        between two matrices
+        Arguments:
+         - X1 is a numpy.ndarray of shape (m, 1)
+         - X2 is a numpy.ndarray of shape (n, 1)
+        Returns:
+         The covariance kernel matrix as a numpy.ndarray of shape (m, n)
         """
-        # m = 0
-        # n = 0
-        # X1 = np.ndarray(shape=(m, 1))
-        # X2 = np.ndarray(shape=(n, 1))
+        sqdist = np.sum(X1**2, 1).reshape(-1, 1) + np.sum(X2**2, 1) - 2 \
+            * np.dot(X1, X2.T)
+        K = self.sigma_f**2 * np.exp(-0.5 / self.l**2 * sqdist)
 
-        # quared distance
-        sqdist = np.sum(X1**2, 1).reshape(-1, 1)\
-            + np.sum(X2**2, 1) - 2 * np.dot(X1, X2.T)
-        covMatrix = self.sigma_f**2 * np.exp(-0.5 / self.L**2 * sqdist)
-        # print(covMatrix)
-        return covMatrix
+        return K

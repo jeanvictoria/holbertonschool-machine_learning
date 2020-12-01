cd350b502456f36e731047888caf499914afba89
Jean Victoria <jean.vic8@gmail.com>
2020-11-30 22:29:46 -0500

    task 1

---
+++ unsupervised_learning/0x04-autoencoders/1-sparse.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+"""Autoencoders"""
+
+import tensorflow.keras as keras
+
+
+def sparse(input_dims, hidden_layers,
+           latent_dims, lambtha):
+    """
+    :param input_dims:is an integer containing
+    the dimensions of the model input
+    :param hidden_layers: is a list containing the number
+     of nodes for each hidden layer in the encoder, respectively
+    :param latent_dims: is an integer containing the
+     dimensions of the latent space representation
+     :param lambtha: is the regularization parameter
+      used for L1 regularization on the encoded output
+    :return:encoder, decoder, auto
+    """
+    input_image = keras.Input(shape=(input_dims,))
+    output = keras.layers.Dense(hidden_layers[0],
+                                activation='relu')(input_image)
+    # encoder
+    for layer in range(1, len(hidden_layers)):
+        output = keras.layers.Dense(hidden_layers[layer],
+                                    activation='relu')(input_image)
+
+    regu = keras.regularizers.l1(lambtha)
+    encoder_out = keras.layers.Dense(latent_dims,
+                                     activation='relu',
+                                     activity_regularizer=regu)(output)
+
+    input_decoder = keras.Input(shape=(latent_dims,))
+    out_decoder = keras.layers.Dense(hidden_layers[-1],
+                                     activation='relu')(input_decoder)
+
+    for layer in range(len(hidden_layers) - 2, -1, -1):
+        out_decoder = keras.layers.Dense(hidden_layers[layer],
+                                         activation='relu')(out_decoder)
+    decoder_out = keras.layers.Dense(input_dims,
+                                     activation='sigmoid')(out_decoder)
+
+    encoder = keras.models.Model(inputs=input_image,
+                                 outputs=encoder_out)
+    decoder = keras.models.Model(inputs=input_decoder,
+                                 outputs=decoder_out)
+
+    input_auto = keras.Input(shape=(input_dims, ))
+
+    full_encoder = encoder(input_auto)
+    full_decoder = decoder(full_encoder)
+    auto = keras.models.Model(inputs=input_auto,
+                              outputs=full_decoder)
+    auto.compile(optimizer='Adam',
+                 loss='binary_crossentropy')
+
+    return encoder, decoder, auto

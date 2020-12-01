a24f90d626cfcf9a79ad255a3e7c258d375933b5
Jean Victoria <jean.vic8@gmail.com>
2020-11-30 22:22:51 -0500

    task 0

--- unsupervised_learning/0x04-autoencoders/0-vanilla.py
+++ unsupervised_learning/0x04-autoencoders/0-vanilla.py
@@ -1,51 +1,57 @@
 #!/usr/bin/env python3
-"""Autoencoders"""
-
+"""
+"Vanilla" Autoencoder
+"""
 import tensorflow.keras as keras
 
 
 def autoencoder(input_dims, hidden_layers, latent_dims):
     """
-    :param input_dims:is an integer containing
-    the dimensions of the model input
-    :param hidden_layers: is a list containing the number
-     of nodes for each hidden layer in the encoder, respectively
-    :param latent_dims: is an integer containing the
-     dimensions of the latent space representation
-    :return:encoder, decoder, auto
+    Function that creates an autoencoder
+    Arguments:
+        - input_dims is an integer containing the dimensions of the model input
+        - hidden_layers is a list containing the number of nodes for each
+        hidden layer in the encoder, respectively
+            * the hidden layers should be reversed for the decoder
+        - latent_dims is an integer containing the dimensions
+        of the latent space representation
+    Returns:
+        encoder, decoder, auto
+        - encoder is the encoder model
+        - decoder is the decoder model
+        - auto is the full autoencoder model
     """
-    input_image = keras.Input(shape=(input_dims,))
+    # Encoder
+    iencoder = keras.Input(shape=(input_dims,))
+
     output = keras.layers.Dense(hidden_layers[0],
-                                activation='relu')(input_image)
-    # encoder
-    for layer in range(1, len(hidden_layers)):
-        output = keras.layers.Dense(hidden_layers[layer],
-                                    activation='relu')(input_image)
-    encoder_out = keras.layers.Dense(latent_dims,
-                                     activation='relu')(output)
+                                activation='relu')(iencoder)
 
-    input_decoder = keras.Input(shape=(latent_dims,))
-    out_decoder = keras.layers.Dense(hidden_layers[-1],
-                                     activation='relu')(input_decoder)
+    for i in range(1, len(hidden_layers)):
+        output = keras.layers.Dense(hidden_layers[i],
+                                    activation='relu')(output)
 
-    for layer in range(len(hidden_layers) - 2, -1, -1):
-        out_decoder = keras.layers.Dense(hidden_layers[layer],
-                                         activation='relu')(out_decoder)
-    decoder_out = keras.layers.Dense(input_dims,
-                                     activation='sigmoid')(out_decoder)
+    oencoder = keras.layers.Dense(latent_dims, activation='relu')(output)
 
-    encoder = keras.models.Model(inputs=input_image,
-                                 outputs=encoder_out)
-    decoder = keras.models.Model(inputs=input_decoder,
-                                 outputs=decoder_out)
+    # Decoder
+    idecoder = keras.Input(shape=(latent_dims,))
+    output2 = keras.layers.Dense(hidden_layers[-1],
+                                 activation='relu')(idecoder)
 
-    input_auto = keras.Input(shape=(input_dims, ))
+    for i in range(len(hidden_layers) - 2, -1, -1):
+        output2 = keras.layers.Dense(hidden_layers[i],
+                                     activation='relu')(output2)
 
-    full_encoder = encoder(input_auto)
-    full_decoder = decoder(full_encoder)
-    auto = keras.models.Model(inputs=input_auto,
-                              outputs=full_decoder)
-    auto.compile(optimizer='Adam',
-                 loss='binary_crossentropy')
+    odecoder = keras.layers.Dense(input_dims, activation='sigmoid')(output2)
+
+    encoder = keras.models.Model(inputs=iencoder, outputs=oencoder)
+    decoder = keras.models.Model(inputs=idecoder, outputs=odecoder)
+
+    out_encoder = encoder(iencoder)
+    out_decoder = decoder(out_encoder)
+
+    # Autoencoder
+    auto = keras.models.Model(inputs=iencoder, outputs=out_decoder)
+    auto.compile(optimizer='Adam', loss='binary_crossentropy')
 
     return encoder, decoder, auto
